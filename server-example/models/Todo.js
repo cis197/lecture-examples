22d1649bffd7f7b1e56a5ad16fec404b630ce27d
Abhi <suriabhinav1997@gmail.com>
2018-02-15 14:26:47 -0500

    added model

--- server-example/models/Todo.js
+++ server-example/models/Todo.js
@@ -1,6 +1,6 @@
 // if time
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/lectureDb');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/lectureDb');
 
 const todoSchema = new mongoose.Schema({
   text: { type: String }
